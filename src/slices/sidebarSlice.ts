8c8e4739573ae5594e869c4a4042eef16f509fc2
violettomania <147735091+violettomania@users.noreply.github.com>
2023-11-21 18:37:41 +0100

    Create sidebarSlice.ts

---
+++ src/slices/sidebarSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { RootState } from '../store/store'
+
+interface SidebarState {
+    value: boolean;
+};
+
+const initialState: SidebarState = {
+    value: false,
+};
+
+export const sidebarSlice = createSlice({
+    name: 'sidebar',
+    initialState,
+    reducers: {
+        show: state => {
+            state.value = true;
+        },
+        hide: state => {
+            state.value = false;
+        },
+    }
+});
+
+export const { show, hide } = sidebarSlice.actions;
+
+export const selectSidebar = (state: RootState) => state.value;
+
+export default sidebarSlice.reducer;
