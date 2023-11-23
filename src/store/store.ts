b65e210de3e3899239eaa4e8a9fc874855cac97b
violettomania <danielpolya@protonmail.com>
2023-11-23 09:15:10 +0100

    chore: re-format files

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
-    reducer: {},
-})
+  reducer: {},
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
